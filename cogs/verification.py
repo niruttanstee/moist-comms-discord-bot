f909d57b1dbbf775971ac6b38361c9ef72c1e40c
Nirutt Anstee <80709800+niruttanstee@users.noreply.github.com>
2022-10-27 16:08:07 +0100

    Verification fix bug
    Fixed the bug where print statement prints out twice.

--- cogs/verification.py
+++ cogs/verification.py
@@ -11,26 +11,32 @@ class Verification(commands.Cog):
     async def on_raw_member_update(before, after):
         """
             Called when member status changes.
-            Check whether the member is pending member verification in bool.
+            Check whether the member has a pending member verification.
             If member is pending = 1, otherwise 0.
 
-            member - the member in question
-            guild - to use the get role function
-            role - to give member the role object
+            is_pending = boolean
+            member = the member in question
+            guild = to use the get role function
+            role = to give member the role object
         """
         member = after
         guild = after.guild
         is_pending = after.pending # check if member has pending verification
 
         if not is_pending:
-            # if not pending give the 'online' role
+            # check if member already has role            
             role = guild.get_role(861049651012435998)
-            await member.add_roles(
+
+            if role not in member.roles:
+                # member does not have role, give role
+                await member.add_roles(
                 role, 
                 reason="Member verified.", 
                 atomic=True)
-            print(f"Verification: {member} given 'online' role.")
+                print(f"Verification: Given 'online' role to {member}")
+
         return
+
 
 def setup(bot):
     """Register the cog."""
